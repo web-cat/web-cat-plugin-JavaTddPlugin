0609cf19f054d59edf6cdf27aa5e9d73f4f79675
s-edwards <edwards@cs.vt.edu>
2007-09-15 02:04:16 +0000

    Fixed boilerplate comments.

--- java-src/net/sf/webcat/StringNormalizer.java
+++ java-src/net/sf/webcat/StringNormalizer.java
@@ -1,3 +1,28 @@
+/*==========================================================================*\
+ |  $Id$
+ |*-------------------------------------------------------------------------*|
+ |  Copyright (C) 2007 Virginia Tech
+ |
+ |  This file is part of Web-CAT.
+ |
+ |  Web-CAT is free software; you can redistribute it and/or modify
+ |  it under the terms of the GNU General Public License as published by
+ |  the Free Software Foundation; either version 2 of the License, or
+ |  (at your option) any later version.
+ |
+ |  Web-CAT is distributed in the hope that it will be useful,
+ |  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ |  GNU General Public License for more details.
+ |
+ |  You should have received a copy of the GNU General Public License
+ |  along with Web-CAT; if not, write to the Free Software
+ |  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ |
+ |  Project manager: Stephen Edwards <edwards@cs.vt.edu>
+ |  Virginia Tech CS Dept, 660 McBryde Hall (0106), Blacksburg, VA 24061 USA
+\*==========================================================================*/
+
 package net.sf.webcat;
 
 import java.util.regex.*;
@@ -52,7 +77,7 @@ import java.util.Map;
  *  </p>
  *
  *  @author  Stephen Edwards
- *  @version 2007.08.16
+ *  @version $Id$
  */
 public class StringNormalizer
     extends ArrayList<StringNormalizer.NormalizerRule>
